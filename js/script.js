fc94eb17b39693ff30454e5f2c9e37bd0fecaf69
egarra <garikhabuzov@gmail.com>
2022-05-12 13:14:59 +0300

    homework_modal_added

--- js/script.js
+++ js/script.js
@@ -90,3 +90,28 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', deadLine);
 });
+
+//modal
+
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close'),
+    descriptionBtn = document.querySelector('.description-btn');
+
+more.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+});
+
+descriptionBtn.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+});
